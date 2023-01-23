d2daff61d7297bd0c324bdce5368f832e2ddfde4
zae2001 <77521630+zae2001@users.noreply.github.com>
2023-01-22 23:29:52 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,16 +5,65 @@ function setup() {
 
 // draw() is called 60 times per second
 function draw() {
-    let hr = hour();
-    let min = minute();
-    let sec = second();
+  //noStroke()
+  background(255);
+  let h = hour()%12;
+  let m = minute();
+  let s = second();
   
-    background(225);
+  //keep track of previous values
+  let ph = 100;
+  let pm = 100;
+  let ps = 100;
+  
+  var printbool = false;
+  if(s==1){
+    printbool=true;
+    if(s==1 && printbool==true){
+      console.log(minute());
+      printbool=false;
+    }
+    else{
+      printbool=false;
+    }
+    
+  }
+  
+  
+  //hour - pink - top
+  if(h!=ph){
+    fill(255,0,255,100);
+    rect(0,600,h*50, -600)
+    ph=h
+  }
+  
+  
+  //minute - yellow - left
+  if(m>0 && m!=pm){
+    fill(255,255,0,120)
+    rect(0,0,600,m*10)
+    pm=m
+  }
+  
+  
+  
+  //second - blue - corner
+  fill(0,255,255,80)
+  triangle(600,600,600,600-((s*10)),600-((s*10)),600)
+  
+  
+  
+  /*template example:
+    //background(225);
     textSize(32);
     fill(180);
-    text(hr, 10, 30);
+    text(h, 10, 30);
     fill(100);
-    text(min, 10, 60);
+    text(m, 10, 60);
     fill(0);
-    text(sec, 10, 90);
+    text(s, 10, 90);*/
 }
+
+
+
+
